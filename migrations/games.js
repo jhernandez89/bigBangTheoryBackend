72b53e5c54dfb29a5db7149c5e5337dc8ddc1c47
Jeff Miguel Hernandez <jeffhernandez89@gmail.com>
2017-11-14 20:54:28 -0600

    migrations added as well as npm start

---
+++ migrations/games.js
@@ -0,0 +1,15 @@
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('games', (table) => {
+    table.string('imageOne');
+    table.string('imageTwo');
+    table.string('title');
+    table.string('description');
+    table.string('videoURL');
+    table.string('amazonLink');
+    table.string('gameLink');
+  });
+};
+
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('games');
+};
